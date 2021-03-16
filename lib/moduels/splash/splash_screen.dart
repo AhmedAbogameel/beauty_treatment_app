0219cf4b9e3fb2906ba79a6b46610d74aea6f759
AhmedAbogameel <gamilco81@gmail.com>
2021-03-16 13:50:22 +0200

    V(1.0.0)

--- lib/moduels/splash/splash_screen.dart
+++ lib/moduels/splash/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState()
   {
     super.initState();
-    Timer(Duration(seconds: 5),()
+    Timer(Duration(seconds: 3),()
     {
       Navigator.push(context, MaterialPageRoute(builder: (context) => currentLoggedUser.isLogged ? HomePageScreen() : introScreen(),));
     });
